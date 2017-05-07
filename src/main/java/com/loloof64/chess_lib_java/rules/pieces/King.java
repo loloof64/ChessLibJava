13860c27dbf100f040cce1690fc4df39585290c7
laurent-bernabe <laurent.bernabe@gmail.com>
2017-05-07 13:28:17 +0200

    Implement basic castle move check : without the 'king in chess' rules.

--- src/main/java/com/loloof64/chess_lib_java/rules/pieces/King.java
+++ src/main/java/com/loloof64/chess_lib_java/rules/pieces/King.java
@@ -2,6 +2,8 @@ package com.loloof64.chess_lib_java.rules.pieces;
 
 import com.loloof64.chess_lib_java.rules.Position;
 import com.loloof64.chess_lib_java.rules.coords.BoardCell;
+import com.loloof64.chess_lib_java.rules.coords.BoardFile;
+import com.loloof64.chess_lib_java.rules.coords.BoardRank;
 
 public class King extends Piece {
 
@@ -15,10 +17,24 @@ public class King extends Piece {
         final int deltaY = to.rank - from.rank;
         final int absDeltaX = Math.abs(deltaX);
         final int absDeltaY = Math.abs(deltaY);
+        final boolean standardMove = (absDeltaX > 0 || absDeltaY > 0) && absDeltaX < 2 && absDeltaY < 2;
+        final int playerBackRank = whitePlayer ? BoardRank.RANK_1.ordinal() : BoardRank.RANK_8.ordinal();
+
+        final boolean noPieceInKingSidePath = position.getPieceAt(new BoardCell(playerBackRank, BoardFile.FILE_F.ordinal())) == null;
+        final boolean hasRightForKingSideCastle = whitePlayer ? position._info.castlesRights.whiteKingSide :
+                position._info.castlesRights.blackKingSide;
+        final boolean kingSideCastleMove = hasRightForKingSideCastle && deltaX == 2 && deltaY == 0 && noPieceInKingSidePath;
+
+        final boolean hasRightForQueenSideCastle = whitePlayer ? position._info.castlesRights.whiteQueenSide :
+                position._info.castlesRights.blackQueenSide;
+        final boolean noPieceInQueenSidePath = position.getPieceAt(new BoardCell(playerBackRank, BoardFile.FILE_D.ordinal())) == null
+                && position.getPieceAt(new BoardCell(playerBackRank, BoardFile.FILE_B.ordinal())) == null;
+        final boolean queenSideCastleMove = hasRightForQueenSideCastle && deltaX == -2 && deltaY == 0 && noPieceInQueenSidePath;
+
         final Piece pieceAtEndCell = position.getPieceAt(to);
         final boolean targetCellHasNoFriendPiece = pieceAtEndCell == null || pieceAtEndCell.isWhitePiece() != whitePlayer;
 
-        return (absDeltaX > 0 || absDeltaY > 0) && absDeltaX < 2 && absDeltaY < 2
+        return (standardMove || kingSideCastleMove || queenSideCastleMove)
                 && targetCellHasNoFriendPiece;
     }
 
--- src/test/java/com/loloof64/chess_lib_java/rules/KingMoveTest.java
+++ src/test/java/com/loloof64/chess_lib_java/rules/KingMoveTest.java
@@ -100,4 +100,64 @@ public class KingMoveTest {
         assertEquals(false, pos6.canMove(BoardCell.D4, BoardCell.E3));
     }
 
+    @Test
+    public void kingCanDoKingSideCastleIfFeatureAvailableAndPathFree(){
+        Position pos1 = Position.fromFEN("r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4");
+        assertEquals(true, pos1.canMove(BoardCell.E1, BoardCell.G1));
+
+        Position pos2 = Position.fromFEN("r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3");
+        assertEquals(false, pos2.canMove(BoardCell.E1, BoardCell.G1));
+
+        Position pos3 = Position.fromFEN("rnbqk1nr/pppp1ppp/8/2b1p3/2B1P3/8/PPPP1PPP/RNBQK1NR w KQkq - 2 3");
+        assertEquals(false, pos3.canMove(BoardCell.E1, BoardCell.G1));
+
+        Position pos4 = Position.fromFEN("rnbqk2r/pppp1ppp/5n2/2b1p3/4P3/3P1N2/PPPN1PPP/R1BQKB1R b KQkq - 2 4");
+        assertEquals(true, pos4.canMove(BoardCell.E8, BoardCell.G8));
+
+        Position pos5 = Position.fromFEN("rnbqkb1r/pppp1ppp/5n2/4p3/4P3/3P1N2/PPP2PPP/RNBQKB1R b KQkq - 0 3");
+        assertEquals(false, pos5.canMove(BoardCell.E8, BoardCell.G8));
+
+        Position pos6 = Position.fromFEN("rnbqk1nr/pppp1ppp/8/2b1p3/4P3/3P1N2/PPP2PPP/RNBQKB1R b KQkq - 0 3");
+        assertEquals(false, pos6.canMove(BoardCell.E8, BoardCell.G8));
+
+        Position pos7 = Position.fromFEN("r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w Qkq - 4 4");
+        assertEquals(false, pos7.canMove(BoardCell.E1, BoardCell.G1));
+
+        Position pos8 = Position.fromFEN("rnbqk2r/pppp1ppp/5n2/2b1p3/4P3/3P1N2/PPPN1PPP/R1BQKB1R b KQq - 2 4");
+        assertEquals(false, pos8.canMove(BoardCell.E8, BoardCell.G8));
+    }
+
+    @Test
+    public void kingCanDoQueenSideCastleIfFeatureAvailableAndPathFree(){
+        Position pos1 = Position.fromFEN("rnbq1rk1/ppp1bppp/3p1n2/4p1B1/4P3/2NP4/PPPQ1PPP/R3KBNR w KQ - 4 6");
+        assertEquals(true, pos1.canMove(BoardCell.E1, BoardCell.C1));
+
+        Position pos2 = Position.fromFEN("rnbq1rk1/pppp1ppp/5n2/2b1p1B1/4P3/2NP4/PPP2PPP/R2QKBNR w KQ - 5 5");
+        assertEquals(false, pos2.canMove(BoardCell.E1, BoardCell.C1));
+
+        Position pos3 = Position.fromFEN("rnbqk2r/ppp1bppp/3p1n2/4p1B1/4P3/3P4/PPPQ1PPP/RN2KBNR w KQkq - 2 5");
+        assertEquals(false, pos3.canMove(BoardCell.E1, BoardCell.C1));
+
+        Position pos4 = Position.fromFEN("rnbq1rk1/ppppbppp/5n2/4p3/4P3/2NP4/PPPQ1PPP/R1B1KBNR w KQ - 5 5");
+        assertEquals(false, pos4.canMove(BoardCell.E1, BoardCell.C1));
+
+        Position pos5 = Position.fromFEN("r3kbnr/pppq1ppp/2np4/4p3/2B1P1b1/P1N2N2/1PPP1PPP/R1BQ1RK1 b kq - 2 6");
+        assertEquals(true, pos5.canMove(BoardCell.E8, BoardCell.C8));
+
+        Position pos6 = Position.fromFEN("r2qkbnr/ppp2ppp/2np4/4p3/2B1P1b1/P4N2/1PPP1PPP/RNBQ1RK1 b kq - 0 5");
+        assertEquals(false, pos6.canMove(BoardCell.E8, BoardCell.C8));
+
+        Position pos7 = Position.fromFEN("rn2kbnr/pppq1ppp/3p4/4p3/2B1P1b1/P4N2/1PPP1PPP/RNBQ1RK1 b kq - 0 5");
+        assertEquals(false, pos7.canMove(BoardCell.E8, BoardCell.C8));
+
+        Position pos8 = Position.fromFEN("r1b1kbnr/pppq1ppp/2np4/4p3/2B1P3/P4N2/1PPP1PPP/RNBQ1RK1 b kq - 0 5");
+        assertEquals(false, pos8.canMove(BoardCell.E8, BoardCell.C8));
+
+        Position pos9 = Position.fromFEN("rnbq1rk1/ppp1bppp/3p1n2/4p1B1/4P3/2NP4/PPPQ1PPP/R3KBNR w K - 4 6");
+        assertEquals(false, pos9.canMove(BoardCell.E1, BoardCell.C1));
+
+        Position pos10 = Position.fromFEN("r3kbnr/pppq1ppp/2np4/4p3/2B1P1b1/P1N2N2/1PPP1PPP/R1BQ1RK1 b k - 2 6");
+        assertEquals(false, pos10.canMove(BoardCell.E8, BoardCell.C8));
+    }
+
 }
